ec50c3df965fe6107061bcb6f9cc597bbcf7a8f8
Bob Stammers <stammers.bob@gmail.com>
2022-04-27 03:28:10 +0100

    Image storage

--- datasnag.go
+++ datasnag.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bufio"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"regexp"
 	"strings"
 
 	yaml "gopkg.in/yaml.v2"
@@ -14,13 +17,73 @@ import (
 var Inpfile = flag.String("in", "\\temp\\overnightoffsite.sql", "The input SQL file")
 var Outfile = flag.String("out", "sqlite.sql", "The output SQL file")
 var bk = flag.Int("bk", 3000, "Size of buffer in 1024 byte chunks")
+var maxInserts = flag.Int("max", -1, "Max number of INSERTs to process")
+var onlyTable = flag.String("table", "", "Only process this table")
+var useTrans = flag.Int("trans", 0, "0=no transactions' 1=single transaction; -1=per table transactions")
+var verbose = flag.Bool("v", false, "Show more messages")
+var imgFolder = flag.String("images", "\\temp\\crap", "Folder to write image files")
+var imgNumber = flag.Int("imgnum", 1, "First number to use for image files")
 
 var yml = `
-DropTables: [archiveddocs,deleteddocs]
+DropTables: [archiveddocs,	# Don't care
+			deleteddocs,
+			"switchboard items",
+			tblletters,		# superseded
+			tletterqq,		# transactional only, bad export
+			tletterq,		# transaction, defunct, bad export
+			tmessages,		# transactional, formatting
+			callblockers,	# defunct
+			cardchecks,
+			cc_notify,		# defunct?
+			cleansebatches,
+			cleansedata,
+			dd_capture, dd_capture_history,
+			dd_history,
+			dd_notify,		# transactional
+			docarchives,
+			fplandata,		# defunct
+			fyearplans,		# defunct
+			qryfetchactivedds2live,
+			rejected_data_records,
+			tblddtransactions,	# transactional
+			tblduplicates,		# defunct
+			tblplankeys,			# looks uninteresting
+			tblsources,			# data transactional
+			paymentplans,		# superseded by tcontracts
+			tmp_itemtable, tmp_live, tmptab,
+			tnewplanq,
+			tplancallouts,
+			tprospectlog,		# defunct
+			tsalesagents,		# defunct
+			xlsimport,
+			]
+
+# These words are not wanted in CREATE statements
+DropWords: 	[unsigned,COMMENT,CHARSET,ENGINE,DEFAULT,AUTO_INCREMENT,NOT,"NULL",latin1,utf8,PRIMARY,UNIQUE,",",ROW_FORMAT,DYNAMIC,CURRENT_TIMESTAMP]
+
+# These words involve skipping the next token as well
+SkipWords: [COMMENT,CHARSET,ENGINE,DEFAULT,AUTO_INCREMENT]
+
+SkipClauses: [PRIMARY,UNIQUE,KEY,ON]
+
+ExtractImageTables: [{ "name":"tcustomerdocs", "ix":4}]
+
+ImageFilename: "img%v.jpg"
 
 `
+
+type ImageTable struct {
+	Tablename    string `yaml:"name"`
+	ImageFieldIx int    `yaml:"ix"`
+}
+
 var cfg struct {
-	DropTables []string `yaml:"DropTables"`
+	DropTables    []string     `yaml:"DropTables"`
+	DropWords     []string     `yaml:"DropWords"`
+	SkipWords     []string     `yaml:"SkipWords"`
+	SkipClauses   []string     `yaml:"SkipClauses"`
+	ImageTables   []ImageTable `yaml:"ExtractImageTables"`
+	ImageFilename string       `yaml:"ImageFilename"`
 }
 
 const appversion = "Datasnag v0.1"
@@ -28,6 +91,10 @@ const appversion = "Datasnag v0.1"
 var mybuffersize = 4000 * 1024
 var W *bufio.Writer
 var CommitNeeded = false
+var CreateInProgress = false
+var SkipNextToken = false
+var CommaWaiting = false
+var ImagesStored = 0
 
 func main() {
 
@@ -41,6 +108,10 @@ func main() {
 	fmt.Printf("%v\n", cfg)
 
 	processFile(*Inpfile)
+	if ImagesStored > 0 {
+		fmt.Printf("%v images stored in %v\n", ImagesStored, *imgFolder)
+		fmt.Printf("Next time use -imgnum %v\n", *imgNumber)
+	}
 
 }
 
@@ -68,6 +139,10 @@ func processFile(f string) {
 	buf := make([]byte, mybuffersize)
 	scanner.Buffer(buf, mybuffersize)
 	var x string
+	numInserts := 0
+	if *useTrans == 1 {
+		W.WriteString("BEGIN TRANSACTION\n")
+	}
 	for scanner.Scan() {
 		x = scanner.Text()
 		xx := strings.Split(x, " ")
@@ -100,29 +175,59 @@ func processFile(f string) {
 		tname := ""
 		if tnameix > 0 && tnameix < lx {
 			tname = xx[tnameix]
-			for _, dt := range cfg.DropTables {
-				if tname == "`"+dt+"`" {
-					processLine = false
+			if *onlyTable != "" && tname != "`"+*onlyTable+"`" {
+				processLine = false
+			} else {
+				for _, dt := range cfg.DropTables {
+					if tname == "`"+dt+"`" {
+						processLine = false
+					}
 				}
 			}
 		}
 		if !processLine {
-			//fmt.Printf("Dropping line belonging to %v\n", tname)
+			if *verbose && xx[0] == "CREATE" {
+				fmt.Printf("Dropping %v\n", tname)
+			}
 			continue
+		}
+		isImg := false
+		imgCol := 0
+		for _, i := range cfg.ImageTables {
+			if tname == "`"+i.Tablename+"`" {
+				isImg = true
+				imgCol = i.ImageFieldIx
+				break
+			}
 		}
 		switch xx[0] {
 		case "CREATE":
+			if *verbose {
+				fmt.Printf("Creating %v\n", tname)
+			}
+			CreateInProgress = true
 			processCreateLine(x)
 		case "DROP":
 			processDropLine(x)
 		case "INSERT":
-			processInsertLine(x)
+			processInsertLine(x, isImg, imgCol)
+			numInserts++
+			if *maxInserts > 0 && numInserts > *maxInserts {
+				return
+			}
 		case "LOCK":
 			processLockLine(x)
 		case "UNLOCK":
 			processUnlockLine(x)
+		default:
+			if CreateInProgress {
+				processCreateLine(x)
+			}
 		}
 
+	}
+	if *useTrans == 1 {
+		W.WriteString("COMMIT;\n")
 	}
 	z := len(x)
 	if z > 100 {
@@ -135,20 +240,106 @@ func processFile(f string) {
 
 func processCreateLine(x string) {
 
+	re := regexp.MustCompile(`(\w+|` + "`" + `[\w\s]+` + "`" + `|'.*'|\(|\)|,)`)
+	rel := regexp.MustCompile(`;$`)
+	CreateInProgress = !rel.MatchString(x)
+	rex := re.FindAllString(x, -1)
+
+	//fmt.Println()
+
+	for _, rx := range rex {
+
+		//fmt.Printf("{%v} ", rx)
+		if rx == "," {
+			SkipNextToken = false
+		}
+		if SkipNextToken {
+			SkipNextToken = false
+			continue
+		}
+		skip := false
+		for _, s := range cfg.SkipClauses {
+			if s == rx {
+				skip = true
+			}
+		}
+		if skip {
+			CommaWaiting = false
+			break
+		}
+
+		if CommaWaiting {
+			CommaWaiting = false
+			W.WriteString(",")
+		}
+
+		for _, s := range cfg.SkipWords {
+			if s == rx {
+				SkipNextToken = true
+			}
+		}
+		drop := false
+		for _, s := range cfg.DropWords {
+			if s == rx {
+				drop = true
+			}
+		}
+		if rx == "," {
+			CommaWaiting = true
+		}
+		if !drop {
+			//fmt.Printf("[.%v.] ", rx)
+			W.WriteString(rx + " ")
+		}
+	}
+	if !CreateInProgress {
+		W.WriteString(";\n")
+	}
+
 }
 
 func processDropLine(x string) {
 
+	W.WriteString("\n" + x)
 }
 
-func processInsertLine(x string) {
+func processInsertLine(x string, img bool, ix int) {
+
+	y := strings.ReplaceAll(x, `\'`, "''")
+	if !img {
+		W.WriteString(y)
+		return
+	}
+	re := regexp.MustCompile(`([^\(]+)\(([^\)]+)`)
+	rex := re.FindStringSubmatch(y)
+	W.WriteString(rex[1] + "(")
+
+	re2 := regexp.MustCompile(`('[^']*'|[^,]*),?`)
+	rey := re2.FindAllStringSubmatch(rex[2], -1)
+	started := false
+	for rx, ry := range rey {
+		if started {
+			W.WriteString(",")
+		}
+		if rx != ix {
+			//fmt.Printf("rey %v: %v\n", rx, ry)
+			W.WriteString(ry[1])
+		} else {
+			imgname := storeImage([]byte(ry[1][2:]))
+			W.WriteString("'" + imgname + "'")
+		}
+		started = true
+	}
+	W.WriteString(");\n")
 
 }
 
 func processLockLine(x string) {
 
-	W.WriteString("BEGIN TRANSACTION;\n")
-	CommitNeeded = true
+	if *useTrans < 0 {
+		W.WriteString("BEGIN TRANSACTION;\n")
+		CommitNeeded = true
+	}
 }
 
 func processUnlockLine(x string) {
@@ -158,4 +349,26 @@ func processUnlockLine(x string) {
 	}
 	CommitNeeded = false
 
+}
+
+func storeImage(hexbytes []byte) string {
+
+	fname := fmt.Sprintf(cfg.ImageFilename, *imgNumber)
+	*imgNumber++
+	fpath := filepath.Join(*imgFolder, fname)
+	f, err := os.Create(fpath)
+	if err != nil {
+		fmt.Printf("can't create %v (%v)\n", fpath, err)
+		return fname
+	}
+	defer f.Close()
+	buf := make([]byte, hex.DecodedLen(len(hexbytes)))
+	_, err = hex.Decode(buf, hexbytes)
+	if err != nil {
+		fmt.Printf("can't decode hex %v [%v]\n", hexbytes[0:20], err)
+		return fname
+	}
+	f.Write(buf)
+	ImagesStored++
+	return fname
 }
